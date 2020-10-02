37a82087dc366df3fef8038f968d5a168249e1ac
Sayansky Nikolay <sarody@ya.ru>
2020-10-02 23:44:17 +0700

    Таймер

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'test';
+  new = false;
+  timer2: string = "Добавить счетчик";
+  list: number[] = [0,0];
+  states: boolean[] = [false,false];
+  dates: any[] = [];
+
+  toggleTimer() {
+    this.new=!this.new;
+    if(this.new){
+      this.timer2 = "Убрать счетчик";
+    }else{
+      this.timer2 = "Добавить счетчик";
+    }
+  }
+
+  catchTimer1(ev) {
+    this.list[0] = ev;
+  }
+
+  catchTimer2(ev) {
+    this.list[1] = ev;
+  }
+
+  stateTimer1(ev) {
+    this.dates[0] = Date();
+    this.states[0] = ev;
+  }
+
+  stateTimer2(ev){
+    this.dates[1] = Date();
+    this.states[1] = ev;
+  }
+
+  logicTimer1() {
+    if (this.dates[1] <= this.dates[0] || this.states[0] == false) {
+      if (this.states[0] == true) {
+        if (this.states[1] == true) {
+          this.states[1] = false;
+        }
+      }else {
+        return false;
+      }
+    }
+  }
+
+  logicTimer2() {
+    if (this.states[1] == true) {
+      if (this.states[0] == true) {
+        this.states[0] = false;
+      }
+    }else {
+      return false;
+    }
+  }
+
+  countTimers() {
+    alert(this.list[0] + this.list[1]);
+    this.states[0] = this.states[1] = false;
+  }
 }
---
+++ src/app/timer/timer.component.ts
@@ -0,0 +1,46 @@
+import {Component, Input, OnInit, Output, EventEmitter, OnChanges, SimpleChanges} from '@angular/core';
+import { timer } from "rxjs";
+
+
+@Component({
+  selector: 'app-timer',
+  templateUrl: './timer.component.html',
+  styleUrls: ['./timer.component.css']
+})
+export class TimerComponent implements OnInit, OnChanges {
+  @Input() timeIn: number;
+  @Input() isOnIn: boolean;
+  @Output() timeOut = new EventEmitter();
+  @Output() isOnOut = new EventEmitter();
+
+  isRunning = false;
+  time: number = 0;
+  timer: string = "Старт";
+
+  ngOnInit(): void {
+    timer(0, 1000).subscribe(ellapsedCycles => {
+      if (this.isRunning) {
+        this.time++;
+        this.timeOut.emit(this.time);
+      }
+    });
+  }
+  ngOnChanges(): void{
+    if(this.isOnIn == false && this.isRunning == true){
+      this.toggleTimer();
+    }
+    if(this.timeIn >= 0) {
+      this.time = this.timeIn;
+    }
+  }
+
+  toggleTimer() {
+    this.isRunning = !this.isRunning;
+    this.isOnOut.emit(this.isRunning);
+    if(this.isRunning) {
+      this.timer = "Пауза";
+    }else {
+      this.timer = "Старт";
+    }
+  };
+}
